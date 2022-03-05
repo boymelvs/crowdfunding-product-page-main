5997c9855f60043e9c5813e7c8c297d177ed3d14
boymelvs <ambo_dacoycoy@yahoo.com>
2022-03-05 22:33:19 +0300

    Initial Commit

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const addRemoveClasses = (value) => {
+   if (value.classList.contains("active")) {
+      value.classList.remove("active");
+
+      /* time delay for removing show classes */
+      setTimeout(() => {
+         value.classList.remove("show");
+      }, 250);
+
+      /* adding show & active classes */
+   } else {
+      value.classList.add("show");
+
+      /* time delay for adding active classes */
+      setTimeout(() => {
+         value.classList.add("active");
+      }, 0.1);
+   }
+};
+
+/* provide looping for multiple elements */
+const selectEachELement = (elements) => {
+   elements.forEach((element) => {
+      addRemoveClasses(element);
+   });
+};
+
+/* if burger is click run this function */
+const clickBurger = (burger, burgerLines, navList) => {
+   const overlay = document.querySelector(".overlay");
+
+   burger.addEventListener("click", () => {
+      selectEachELement(burgerLines);
+      addRemoveClasses(navList);
+      addRemoveClasses(overlay);
+   });
+};
+
+/* get burger element & nav list */
+const burger = document.querySelector(".burger_menu");
+const burgerLines = document.querySelectorAll(".burger_line");
+const navList = document.querySelector(".nav_list");
+
+clickBurger(burger, burgerLines, navList);
